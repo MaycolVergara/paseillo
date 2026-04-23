cf2736328aa788e0cbb5467944d2dbe15cd08a00
Maycol Vergara <maycolvergara123@gmail.com>
2026-04-22 20:23:45 -0500

    Tomar Fotos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolSeeder::class,
-            CategorySeeder::class,
-            ProductSeeder::class,
+           // CategorySeeder::class,
+            //ProductSeeder::class,
             StaffSeeder::class,
-            SettingSeeder::class,
-            TableSeeder::class,
-            TableDeliverySeeder::class,
+            //SettingSeeder::class,
+            //TableSeeder::class,
+            //cTableDeliverySeeder::class,
         ]);
     }
 }
